7c572277c1896bd124d6aa7966d4b1f032d2bf24
ajay.jangid <ajay.jangid@radixweb.com>
2023-08-17 15:09:16 +0530

    test11

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use((req, res, next) => {
 app.get("/ping", (req, res, next) => {
   return res
     .status(200)
-    .json({ ok: "Ok with ci-cd test----&&&---_____*******------>" });
+    .json({ ok: "Ok with ci-cd test----&&&---yyy_____*******------>" });
 });
 
 app.use("/products", productsRoutes);
