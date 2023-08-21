875c8c63ee0f17276a8b49b03c8a678948a4e78e
ajay.jangid <ajay.jangid@radixweb.com>
2023-08-21 17:32:12 +0530

    ok fine

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use((req, res, next) => {
 
 // Routes which should handle requests
 app.get("/ping", (req, res, next) => {
-  return res.status(200).json({ ok: "Ok with ci-cd abhishek>" });
+  return res.status(200).json({ ok: "Ok with ci-cd abhishek_____________->" });
 });
 
 app.use("/products", productsRoutes);
